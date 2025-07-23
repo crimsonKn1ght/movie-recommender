3ec34228e788ffb5aed8c7ee781f38a873860283
crimsonKn1ght <87464911+crimsonKn1ght@users.noreply.github.com>
2025-07-23 15:51:26 +0530

    resolved workflow issues

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders movie recommender title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Advanced Movie Recommender/i);
+  expect(titleElement).toBeInTheDocument();
 });
